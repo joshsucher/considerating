452c963449ff890465adc9758448d6138ad4b5a3
Danielle Sucher <dsucher@gmail.com>
2012-02-20 23:38:49 -0500

    Tried forcing approval prompt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
 	
 	def destroy
 		session[:user_id] = nil
-		// redirect_to "https://accounts.google.com/accounts/Logout"
 		redirect_to root_url
 	end
 end
