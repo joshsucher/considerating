df4f70df231cd0eb588fbd73f906e42746c48d6b
Danielle Sucher <dsucher@gmail.com>
2012-02-23 01:59:33 -0500

    Moved modernizr call

--- app/assets/javascripts/documentready.js
+++ app/assets/javascripts/documentready.js
@@ -1,3 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function() {  
+	if (!Modernizr.inputtypes.range) {    
+		$('#new-vote-<%= @consideration.id %>').html("<%= escape_javascript(render('shared/dropdown_voting_logged_out')) %>"); 
+	}
 	$(":range").rangeinput();
 });
