2b5a5b80e80308002e525abd0a9cde7bcae1751c
Danielle Sucher <dsucher@gmail.com>
2012-02-23 15:10:47 -0500

    Changed voting to dropdown conditioned on modernizr and restyled permalink

--- app/assets/javascripts/documentready.js
+++ app/assets/javascripts/documentready.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {  
-	if (!Modernizr.inputtypes.range) {    
-		$(".modern_logged_out:input[name='rating']").html("<select name='rating'><option value='10'>considerate</option><option value='20'>2</option><option value='30'>3</option><option value='40'>4</option><option value='50'>5</option><option value='60'>6</option><option value='70'>7</option><option value='80'>4</option><option value='90'>9</option><option value='100'>creepy</option></select>"); 
+	if(!Modernizr.inputtypes.range) {
+		$(".rating_logged_out").html("<br /><select name='rating'><option value='10'>considerate</option><option value='20'>2</option><option value='30'>3</option><option value='40'>4</option><option value='50'>5</option><option value='60'>6</option><option value='70'>7</option><option value='80'>8</option><option value='90'>9</option><option value='100'>creepy</option></select>"); 
+		$(".rating_logged_in").html("<br /><select name='rating'><option value='1.0'>considerate</option><option value='2.0'>2</option><option value='3.0'>3</option><option value='4.0'>4</option><option value='5.0'>5</option><option value='6.0'>6</option><option value='7.0'>7</option><option value='8.0'>8</option><option value='9.0'>9</option><option value='10.0'>creepy</option></select>"); 
 	}
 	$(":range").rangeinput();
 });
