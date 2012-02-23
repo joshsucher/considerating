56125ee21aaf624783bb47690addadeeb84c8b55
Danielle Sucher <dsucher@gmail.com>
2012-02-23 02:31:17 -0500

    Testing modernizr

--- app/assets/javascripts/documentready.js
+++ app/assets/javascripts/documentready.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {  
 	if (!Modernizr.inputtypes.range) {    
-		$('#new-vote-<%= @consideration.id %>').html("<%= escape_javascript(render('shared/dropdown_voting_logged_out')) %>"); 
+		$(".modern_logged_out:input[name='rating']").html("<select name='rating'><option value='10'>considerate</option><option value='20'>2</option><option value='30'>3</option><option value='40'>4</option><option value='50'>5</option><option value='60'>6</option><option value='70'>7</option><option value='80'>4</option><option value='90'>9</option><option value='100'>creepy</option></select>"); 
 	}
 	$(":range").rangeinput();
 });
